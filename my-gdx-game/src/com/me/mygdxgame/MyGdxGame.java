2ce8d0b5477d3895cbde52c86b966168082b98f7
Hema Vidal <hemavidal@gmail.com>
2013-07-29 18:11:46 -0300

    Organizando os atributos da Classe MyGdxGame.java

--- my-gdx-game/src/com/me/mygdxgame/MyGdxGame.java
+++ my-gdx-game/src/com/me/mygdxgame/MyGdxGame.java
@@ -1,53 +1,18 @@
 package com.me.mygdxgame;
 
-import aurelienribon.bodyeditor.BodyEditorLoader;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.physics.box2d.joints.FrictionJoint;
-import com.badlogic.gdx.physics.box2d.joints.FrictionJointDef;
-import com.badlogic.gdx.physics.box2d.joints.MouseJoint;
-import com.badlogic.gdx.physics.box2d.joints.MouseJointDef;
 
 public class MyGdxGame implements ApplicationListener {
-	private Vector2 gravity;
 	private TampinhaWorld tampinhaWorld;
 	private Box2DDebugRenderer debugRenderer;
 	public static OrthographicCamera camera;
-	private Body currentBody;
-	private Vector2 currentMousePosition;
-	public static MouseJoint mousejoint;
-	private MouseJointDef mouseJointDef;
-	private Body mousePointBody;
-	private BodyDef boxBodyDef;
-	private Body boxBody;
-	private FrictionJoint frictionJoint;
-	private BodyDef mousePointBodyDef;
-	private Texture texture;
-	private Sprite sprite;
-	private SpriteBatch spriteBatch;
-	private Tampa tampa;
-	private Pista pista;
 	
 	@Override
 	public void create() {
-		
-
 		//Create World
 		tampinhaWorld = new TampinhaWorld();
 		
