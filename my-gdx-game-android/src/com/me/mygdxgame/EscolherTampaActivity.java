d9a97ea1f896e0e9a4c0c75baa6e1292505bfdd4
Renan Diniz <renan.almeida@ccc.ufcg.edu.br>
2013-08-20 14:34:57 -0300

    codigo do jogo integrado ao menu

--- my-gdx-game-android/src/com/me/mygdxgame/EscolherTampaActivity.java
+++ my-gdx-game-android/src/com/me/mygdxgame/EscolherTampaActivity.java
@@ -1,20 +1,21 @@
 package com.me.mygdxgame;
 
-
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class EscolherTampaActivity extends Activity{
+public class EscolherTampaActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_escolhertampa);
-		
+
 		Button butaoVoltar = (Button) findViewById(R.id.butao_voltarTampa);
 		butaoVoltar.setOnClickListener(new OnClickListener() {
 
@@ -23,18 +24,18 @@ public class EscolherTampaActivity extends Activity{
 				finish();
 			}
 		});
-		
 
 		Button butaoTampa1 = (Button) findViewById(R.id.butao_tampa1);
 		butaoTampa1.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				 new MainActivity();
-
+				Intent inciarJogo = new Intent(EscolherTampaActivity.this,
+						GameActivity.class);
+				startActivity(inciarJogo);
 			}
 		});
-		
+
 		Button butaoTampa2 = (Button) findViewById(R.id.butao_tampa2);
 		butaoTampa2.setOnClickListener(new OnClickListener() {
 
@@ -43,9 +44,6 @@ public class EscolherTampaActivity extends Activity{
 				System.out.println("jogar com tampa 2");
 			}
 		});
-		
-		
-		
-		
+
 	}
 }
--- my-gdx-game-android/src/com/me/mygdxgame/MainActivity.java
+++ my-gdx-game-android/src/com/me/mygdxgame/MainActivity.java
@@ -1,19 +1,51 @@
 package com.me.mygdxgame;
 
+import com.me.mygdxgame.R;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+public class MainActivity extends Activity {
+	private Intent intentOpcoes;
+	private Intent intentJogar;
 
-public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
-        
-        initialize(new MyGdxGame(), cfg);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button butaoJogar = (Button) findViewById(R.id.butao_jogar);
+		butaoJogar.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				if (intentJogar == null) {
+					intentJogar = new Intent(MainActivity.this, JogarActivity.class);
+				}
+				startActivity(intentJogar);
+
+			}
+		});
+		Button butaoOpcoes = (Button) findViewById(R.id.butao_opcoes);
+		butaoOpcoes.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				if (intentOpcoes == null) {
+					intentOpcoes = new Intent(MainActivity.this, OpcoesActivity.class);
+				}
+				startActivity(intentOpcoes);
+			}
+		});
+		Button butaoSair = (Button) findViewById(R.id.butao_sair);
+		butaoSair.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				System.exit(0);
+
+			}
+		});
+	}
+
+	
+
 }
